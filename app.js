604cf573fdc11539136a0590d1ea5623d7bf6b47
UzainAli5660 <162098638+UzainAli5660@users.noreply.github.com>
2024-03-08 22:35:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+// alert
+alert ( "Hello World" )
+alert ( "This is Uzain Ali's Website")
+
+
+// variables
+var userName = "Uzain Ali";
+console.log (userName);
+var age = 16;
+console.log ( age);
+
+// Boolean/
+var isActive = true
+console.log (isActive);
+
+// Null
+var email = null;
+console.log ( email );
+
+// Undefined 
+var result = undefined;
+console.log ( result);
+
+
+
+// Counting Of Variables
+var a = 30
+var b = 60
+var add= a+b;
+console.log ( add );
+
+var firstName = "Uzain";
+var lastName = "Ali";
+var add = firstName+lastName;
+console.log ( add );
+
+// Operators
+var a=10
+console.log ( a + a++ + ++a - a + a++ + ++a - a--)
+
+// prompts
+var userName = prompt("EnterYourName")
+
+
+
+// Conditions
+var x= prompt("Where you live?");
+if (x=== "Karachi") {
+    alert("correct")
+}
